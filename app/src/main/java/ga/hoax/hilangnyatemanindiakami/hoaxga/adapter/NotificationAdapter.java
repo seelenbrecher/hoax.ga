e90318c77a03405db7cf8f9893075d14afbcfbdb
seelenbrecher <maulanaanab@gmail.com>
2016-08-30 13:12:53 +0700

    fix smth anab

--- app/src/main/java/ga/hoax/hilangnyatemanindiakami/hoaxga/adapter/NotificationAdapter.java
+++ app/src/main/java/ga/hoax/hilangnyatemanindiakami/hoaxga/adapter/NotificationAdapter.java
@@ -1,7 +1,11 @@
 package ga.hoax.hilangnyatemanindiakami.hoaxga.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.format.DateUtils;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,10 +31,7 @@ public class NotificationAdapter extends BaseAdapter {
     private Context context;
     private List<Notification> notificationList;
     private ImageView notificationImageView;
-    private TextView userTextView;
-    private TextView typeTextView;
-    private TextView postTextView;
-    private TextView voteTextView;
+    private TextView userContentView;
     private TextView dateTextView;
 
     private DataService dataService;
@@ -64,27 +65,44 @@ public class NotificationAdapter extends BaseAdapter {
             convertView = LayoutInflater.from(context).inflate(R.layout.layout_list_notification, null);
         }
 
-        notificationImageView = (ImageView) convertView.findViewById(R.id.notificationImage);
-        userTextView = (TextView) convertView.findViewById(R.id.otherUserNotificationText);
-        typeTextView = (TextView) convertView.findViewById(R.id.typeNotificationText);
-        postTextView = (TextView) convertView.findViewById(R.id.postNotificationText);
-        voteTextView = (TextView) convertView.findViewById(R.id.voteUserNotificationText);
+        //notificationImageView = (ImageView) convertView.findViewById(R.id.notificationImage);
+        userContentView = (TextView) convertView.findViewById(R.id.notificationContent);
         dateTextView = (TextView) convertView.findViewById(R.id.dateNotificationText);
 
         Notification notification = (Notification) getItem(position);
         User otherUser = userService.getUserByUsername(notification.getOtherUser());
         Post post = dataService.getSinglePost(notification.getPostId());
 
-        notificationImageView.setImageBitmap(userService.getProfileImage(otherUser));
-        userTextView.setText(otherUser.getFirstName());
+        //set notifier image
+        //if(userService.getProfileImage(otherUser) != null) notificationImageView.setImageBitmap(userService.getProfileImage(otherUser));
+
+
+        //notification content
+        String userName;
+        String commentType = "";
+        String commentContent = "";
+        String votePost = "";
+
+        userName = otherUser.getFirstName();
 
         switch (notification.getNotificationType()) {
             case COMMENT:
-                typeTextView.setText("commented on your post");
+                commentType = "commented on your post";
+                commentContent  = "\"" + post.getTitle() + "\"";
                 break;
         }
 
-        postTextView.setText("\"" + post.getTitle() + "\"");
+        //contentView
+        int pos = 0;
+        Spannable contentString = new SpannableString(userName + " " + commentType + " " + commentContent);
+        contentString.setSpan(new ForegroundColorSpan(context.getResources().getColor(R.color.colorPrimary)), pos, pos + userName.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        pos += userName.length()+1;
+        pos += commentType.length()+1;
+        contentString.setSpan(new ForegroundColorSpan(context.getResources().getColor(R.color.colorPrimary)), pos, pos + commentContent.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        pos += commentContent.length()+1;
+        Spannable voteString = new SpannableString(votePost);
+        voteString.setSpan(new ForegroundColorSpan(context.getResources().getColor(R.color.colorAccent)), 0, votePost.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        userContentView.setText(contentString);
 
         Date date = new Date();
         long diff = date.getTime() - post.getDate().getTime();
